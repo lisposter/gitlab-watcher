10c7f874f2388b7afc0430f95ae2312f9a916135
Leigh Zhu <i@zhuli.me>
2015-05-15 13:19:01 +0800

    add app menu

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var BrowserWindow = require('browser-window');
 var appIcon = null;
 app.on('ready', function(){
   appIcon = new Tray('resources/icon.png');
-  var contextMenu = Menu.buildFromTemplate([
+  var trayContextMenu = Menu.buildFromTemplate([
     {
       label: 'Preferences',
       click: openConfig
@@ -24,11 +24,128 @@ app.on('ready', function(){
     }
   ]);
   appIcon.setToolTip('I am watching GitLab for you!');
-  appIcon.setContextMenu(contextMenu);
+  appIcon.setContextMenu(trayContextMenu);
 });
 
 var configWindow = null;
 function openConfig() {
+  var appMenu = Menu.buildFromTemplate([
+    {
+      label: 'ReadmeReader',
+      submenu: [
+        {
+          label: 'Services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Hide',
+          accelerator: 'Command+H',
+          selector: 'hide:'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          selector: 'hideOtherApplications:'
+        },
+        {
+          label: 'Show All',
+          selector: 'unhideAllApplications:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() { app.quit(); }
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'Command+Z',
+          selector: 'undo:'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+Command+Z',
+          selector: 'redo:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'Command+X',
+          selector: 'cut:'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'Command+C',
+          selector: 'copy:'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'Command+V',
+          selector: 'paste:'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:'
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'Command+R',
+          click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+        },
+        {
+          label: 'Toggle DevTools',
+          accelerator: 'Alt+Command+I',
+          click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+        }
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'Command+M',
+          selector: 'performMiniaturize:'
+        },
+        {
+          label: 'Close',
+          accelerator: 'Command+W',
+          selector: 'performClose:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Bring All to Front',
+          selector: 'arrangeInFront:'
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: []
+    }
+  ]);
+  Menu.setApplicationMenu(appMenu);
+
   configWindow = new BrowserWindow({width: 800, height: 600});
 
   configWindow.loadUrl('file://' + __dirname + '/index.html');
