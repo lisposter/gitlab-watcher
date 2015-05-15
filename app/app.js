ab8fcf60cc78078d5a0a10ba34b2d8da1bf5f139
Leigh Zhu <i@zhuli.me>
2015-05-15 12:02:53 +0800

    update app title

--- app/app.js
+++ app/app.js
@@ -1,13 +1,20 @@
 'use strict';
 
-angular.module('gitlab', ['ngRoute', 'gitlab.config'])
+angular.module('gitlab', ['ngRoute', 'gitlab.config', 'ngMaterial'])
 
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+.config(['$mdThemingProvider', '$mdIconProvider', '$routeProvider', '$locationProvider', function($mdThemingProvider, $mdIconProvider, $routeProvider, $locationProvider) {
+
   $routeProvider
    .when('/prefs/', {
-    templateUrl: 'app/config/config.html',
-    controller: 'PrefsCtrl'
-  })
+      templateUrl: 'app/config/config.html',
+      controller: 'PrefsCtrl'
+    })
+    .otherwise('/prefs/');
 
-  .otherwise('/prefs/');
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('red');
+
+  $mdIconProvider
+    .icon("menu"       , "resources/menu.svg"        , 24);
 }]);
