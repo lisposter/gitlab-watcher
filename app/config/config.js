a73ce3ca0c5a46e19b880cde5a22a665bdc976e2
Leigh Zhu <i@zhuli.me>
2015-05-15 13:18:19 +0800

    add config

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,18 @@
-angular.module('gitlab.config', [])
-  .controller('PrefsCtrl', ['$http', '$scope', function($http, $scope) {
+'use strict';
 
-  }]);
+var remote = require('remote');
+var fs = require('fs');
+var app = remote.require('app');
+
+var configPath = app.getPath('userData') + '/config.json';
+
+angular.module('gitlab.config', [])
+.controller('PrefsCtrl', ['$http', '$scope', function($http, $scope) {
+
+  $scope.gitlab = fs.existsSync(configPath) ? require(configPath) : {};
+
+  $scope.savePrefs = function() {
+    fs.writeFileSync(configPath, JSON.stringify($scope.gitlab) || {});
+  };
+
+}]);
