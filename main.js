f4513d0485a08dc1a27ae89367b8ca5bbf893596
Leigh Zhu <i@zhuli.me>
2015-05-17 21:38:05 +0800

    keep app still running when all windows are closed

--- main.js
+++ main.js
@@ -5,10 +5,18 @@ var app = require('app');
 var menu = require('./lib/menu');
 var watcher = require('./lib/watcher');
 
+// hide dock icon
+app.dock.hide();
+
 app.on('ready', function(){
   menu.tray();
   menu.app();
 
   // start watcher
   watcher.watching();
+});
+
+// prevent app close when all windows are closed
+app.on('will-quit', function(e) {
+  e.preventDefault();
 });
