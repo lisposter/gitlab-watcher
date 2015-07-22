92fc3174c7e6cd00dc04ee29a734e14d5aa383c1
Leigh Zhu <i@zhuli.me>
2015-07-22 15:50:38 +0800

    rewrite app.js

--- app/app.js
+++ app/app.js
@@ -1,47 +1,56 @@
-'use strict';
+(function() {
+  'use strict';
 
-var remote = require('remote');
-var app = remote.require('app');
-var fs = require('fs');
+  const remote = require('remote');
+  const app = remote.require('app');
+  const fs = require('fs');
 
-angular.module('gitlab', ['ngRoute', 'gitlab.config', 'ngMaterial'])
+  angular
+    .module('gitlab', [
+      'ngRoute', 'gitlab.config', 'ngMaterial'
+    ])
 
-.config(['$mdThemingProvider', '$mdIconProvider', '$routeProvider', '$locationProvider', function($mdThemingProvider, $mdIconProvider, $routeProvider, $locationProvider) {
+    .config(appConfig)
+    .run(appInit)
+    .directive('scrollEnd', scrollEnd);
 
-  $routeProvider
-   .when('/prefs/', {
-      templateUrl: 'app/config/config.html',
-      controller: 'PrefsCtrl',
-      controllerAs: 'vm'
-    })
-    .otherwise('/prefs/');
+    function appConfig($mdThemingProvider, $mdIconProvider, $routeProvider, $locationProvider) {
+      $routeProvider
+       .when('/prefs/', {
+          templateUrl: 'app/config/config.html',
+          controller: 'PrefsCtrl',
+          controllerAs: 'vm'
+        })
+        .otherwise('/prefs/');
 
-  $mdThemingProvider.theme('default')
-    .primaryPalette('blue')
-    .accentPalette('red');
+      $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('red');
 
-  $mdIconProvider
-    .icon("menu"       , "resources/menu.svg"        , 24);
-}])
+      $mdIconProvider
+        .icon("menu"       , "resources/menu.svg"        , 24);
+    }
 
-.run(['$http', '$rootScope', function($http, $rootScope) {
-  var configPath = app.getPath('userData') + '/config.json';
-  var gitlab = fs.existsSync(configPath) ? require(configPath) : {};
+    function appInit($http, $rootScope) {
+      var configPath = app.getPath('userData') + '/config.json';
+      var gitlab = fs.existsSync(configPath) ? require(configPath) : {};
 
-  if (gitlab.token) {
-    $http.defaults.headers.common['PRIVATE-TOKEN'] = gitlab.token;
-  }
-}])
+      if (gitlab.token) {
+        $http.defaults.headers.common['PRIVATE-TOKEN'] = gitlab.token;
+      }
+    }
 
-.directive('scrollEnd', function() {
-    return function(scope, elm, attr) {
-        var raw = elm[0];
+    function scrollEnd() {
+      return function(scope, elm, attr) {
+          var raw = elm[0];
 
-        elm.bind('scroll', function() {
-          console.log('heh');
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.scrollEnd);
-            }
-        });
-    };
-});
+          elm.bind('scroll', function() {
+            console.log('heh');
+              if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                  scope.$apply(attr.scrollEnd);
+              }
+          });
+      };
+    }
+
+})();
