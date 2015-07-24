8b74ac43f97841c1928ea279116cc2b2e800c7c5
Leigh Zhu <i@zhuli.me>
2015-07-24 16:20:29 +0800

    add comments

--- app/config/config.service.js
+++ app/config/config.service.js
@@ -19,24 +19,42 @@
         loadRepos: loadRepos
       };
 
+      /**
+       * load app config
+       * @return {Object} App config
+       */
       function loadConfig() {
         return fs.existsSync(configPath) ? require(configPath) : {};
       }
 
+      /**
+       * save app config
+       * @param  {Object} config config info
+       */
       function saveConfig(config) {
         if (config.token) {
           $http.defaults.headers.common['PRIVATE-TOKEN'] = config.token;
         }
 
-        // write into configsave
+        // write into config
         fs.writeFileSync(configPath, JSON.stringify(config) || {});
       }
 
+      /**
+       * load repos
+       * @param  {Number||String} pageNum the page number want to load
+       * @return {Promise}         Resolved data
+       */
       function loadRepos(pageNum) {
         pageNum = pageNum || 1;
         return $http.get(gitlab.api + '/projects?order_by=last_activity_at&page=' + pageNum)
           .then(loadComplete);
 
+        /**
+         * processing response data
+         * @param  {Object} response response object from $http.get()
+         * @return {Object}          repos and pager info
+         */
         function loadComplete(response) {
           return {
             repos: response.data,
@@ -44,6 +62,12 @@
           };
         }
 
+
+        /**
+         * get pager info
+         * @param  {String} string from response's http header
+         * @return {Object}        Pager info
+         */
         function getPageNum(string) {
           if (!string) {
             return;
